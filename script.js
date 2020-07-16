46845983c9f3f8cf64aec4ac0f93442929886ca9
Magdalena Krzyzanowski <Magda@Magdalenas-iMac.fios-router.home>
2020-07-16 14:48:42 -0400

    Add JS code to show only one object from JSON data to prepare to write a function for responsive Desktop view.

--- script.js
+++ script.js
@@ -17,44 +17,6 @@ $(document).ready(function() {
 });
 const url = 'https://spreadsheets.google.com/feeds/list/1E2ridB2TyMGdd5tduhDNsan5KZvByoGgD5jonASuTuI/od6/public/values?alt=json'
 
-// fetch(url)
-//     .then(response => response.json())
-//     .then(data => {
-        
-//         // console.log(data)
-//         console.log(data.feed.entry)
-//         const projects = data.feed.entry.map(entry => {
-//             return {
-//                 title: entry.gsx$title.$t,
-//                 image: entry.gsx$image.$t,
-//                 description: entry.gsx$description.$t,
-//                 url: entry.gsx$url.$t
-
-//             }
-//         })
-//         app(projects)
-//     })
-
-// const app = (data) => {
-//     console.log('app is running!')
-//     console.log(data)
-
-//     const createProjectElement = (project) => {
-//         const $div = $('<div>')
-//         $div.append($('<h2>').text(project.title))
-//         $div.append($('<p>').text(project.description))
-//         $div.append($('<img>').attr('src',project.image))
-//         $div.append($('<a>').attr('href',project.url).text('link>>>'))
-//         return $div
-//     }
-
-//         data.forEach( project => {
-//             const $projectDiv = createProjectElement(project)
-//             $('body').append($projectDiv)
-//         })
-
-
-// }}
 
 fetch(url)  // starts the fetch process
     .then( response => response.json() )    // returns the JSON data as a JS object
@@ -73,6 +35,10 @@ fetch(url)  // starts the fetch process
          app(projects)  // calls the application with your new projects array as argument
 
     })
+
+
+
+
 
 // all application logic is included inside the app() function    
 const app = (projects) => {
@@ -93,32 +59,35 @@ const app = (projects) => {
         $div.append($('<p>').addClass("description").text(project.description))
         $div.append($('<button>').append($('<a>').attr('href', project.url).addClass("list-group-item").append($('<i>').addClass("fa fa-codepen").append($('<a>').addClass("small").text('Live Demo')))))
 
-        // <a class="list-group-item" href="#"><i class="fa fa-codepen" aria-hidden="true"></i></a>
-        // $div.append($('<a>').attr('href', project.url).text("LINK >>"))
+
         return $div
     }
 
     // adds each project element to <body>
 
 
+    
 
-    projects.forEach(project => {
+//     projects.forEach(project => {
+        
+//         const $projectDiv = createProjectElement(project)
+//         $('#Projects').append($projectDiv)
+//     })
+
+// }
+
+
+    projects.forEach((project,index) => {
+        
+        if (index === 0) {
         const $projectDiv = createProjectElement(project)
         $('#Projects').append($projectDiv)
+        }
+
+        else {
+        console.log(index)
+        }
     })
 
 }
-
-// function dropDown () {
-//     document.querySelector('.dropDownMenu').style.diplay = 'none';
-    
-//  };
-
-//  function sayHello() {
-//     console.log("Hello!");
-// }
-
-// document.querySelector('#bars').addEventListener('click', sayHello);
-
-// // document.querySelector('.fas\ fa-bars').onclick = dropDown();
 
